890de05961b505bf3387492ad317708167c56f95
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-02-01 16:14:42 +0530

    Delete Tor.py

--- .img/Tor.py
+++
@@ -1,7 +0,0 @@
-import socks
-import socket
-import requests
-
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
-socket.socket = socks.socksocket
-print(requests.get("http://ipecho.net/plain").text)
